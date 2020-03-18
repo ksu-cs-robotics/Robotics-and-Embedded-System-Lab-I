ab3d58d31c0627d27b49fdafed96e5ea42143048
Jong-Hoon_Kim <hoonywiz@github.com>
2020-03-18 07:09:54 -0400

    update 03/18/2020

--- src/RES_Rotary_Encoder_Basic/RES_Rotary_Encoder_Basic.ino
+++ src/RES_Rotary_Encoder_Basic/RES_Rotary_Encoder_Basic.ino
@@ -22,8 +22,7 @@
 *   reference website ( https://www.pjrc.com/teensy/td_libs_Encoder.html )                    
 ***********************************************************************/
 
-#include <Encoder.h>
-
+//#include <Encoder.h>
 // Change these pin numbers to the pins connected to your encoder.
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
@@ -31,12 +30,20 @@
 //Encoder knobLeft(18,19);
 //Encoder knobRight(20, 21);
 //   avoid using pins with LEDs attached
+// 
+
+#define PIN_WHEEL_ENCODER_CLK_LEFT    2
+#define PIN_WHEEL_ENCODER_DT_LEFT    4
+#define PIN_WHEEL_ENCODER_SW_LEFT    6
+#define PIN_MOTOR_PWM_LEFT    8
+#define PIN_MOTOR_IN0_LEFT    9
+#define PIN_MOTOR_IN1_LEFT    10
 
 void setup() {
   Serial.begin(9600);
   Serial.println("TwoKnobs Encoder Test:");
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), updateEncoder, CHANGE);  
+  pinMode(PIN_WHEEL_ENCODER_CLK_LEFT, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_WHEEL_ENCODER_CLK_LEFT), updateEncoder, CHANGE);  
 }
 
 
@@ -57,8 +64,8 @@ void loop() {
 
 
 void updateEncoder(){
-  encoderPin_X = digitalRead(2);
-  encoderPin_Y = digitalRead(9);
+  encoderPin_X = digitalRead(PIN_WHEEL_ENCODER_CLK_LEFT);
+  encoderPin_Y = digitalRead(PIN_WHEEL_ENCODER_DT_LEFT);
 
   if (encoderPin_X == 0){   
     if (encoderPin_Y == 0){
