827519d51a8aca83b37282940b0fa4265e4c1a90
Jong-Hoon Kim <hoonywiz@users.noreply.github.com>
2022-11-07 10:16:18 -0500

    Update RES_Motor_Control_Basic.ino

--- src/RES_Motor_Control_Basic/RES_Motor_Control_Basic.ino
+++ src/RES_Motor_Control_Basic/RES_Motor_Control_Basic.ino
@@ -23,13 +23,13 @@
 ***********************************************************************/
 
 #define PIN_SERVO_0           12
-#define PIN_MOTOR_PWM_LEFT    11
-#define PIN_MOTOR_IN0_LEFT    10
-#define PIN_MOTOR_IN1_LEFT    9
+#define PIN_MOTOR_PWM_LEFT    5
+#define PIN_MOTOR_IN0_LEFT    4
+#define PIN_MOTOR_IN1_LEFT    3
 #define PIN_JOY_SW            A0
 #define PIN_JOY_X             A1
 #define PIN_JOY_Y             A2
-#define INTERVAL_PRINT        1000
+#define INTERVAL_PRINT        100000
 
 
 long preTime = 0;
@@ -48,9 +48,11 @@ void loop() {
   if (preTime > millis()){
       preTime = millis() + INTERVAL_PRINT;
       Serial.print("Left Motor Power  = ");
+      Serial.println(motorSpeedLeft);
+    
   }
 
-  motorSpeedLeft = map( analogRead(PIN_JOY_X), 0,1023, minOut, maxOut):
+  motorSpeedLeft = map( analogRead(PIN_JOY_X), 0,1023, -255, 255);
   if (motorSpeedLeft < 0){
     analogWrite(PIN_MOTOR_PWM_LEFT, -motorSpeedLeft);   digitalWrite( PIN_MOTOR_IN0_LEFT, HIGH);    digitalWrite( PIN_MOTOR_IN1_LEFT , LOW);
     
